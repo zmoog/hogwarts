a26686b1b1eadfbc62b18e1f8640feffc5a4045f
Maurizio Branca <maurizio.branca@gmail.com>
2020-02-02 07:51:39 +0100

    Cleanup commented code

--- test/handlers/test_publish_latest_grades.py
+++ test/handlers/test_publish_latest_grades.py
@@ -1,7 +1,6 @@
 from app.handlers import PublishLatestGradesHandler
 from app.scrapers import GradeScraper
 from app.models import Grade, Student
-# from app.notification.telegram import TelegramNotifier
 
 data_path = f"./testdata/placebo/app/handlers/{__name__}"
 
@@ -93,75 +92,4 @@ Ecco gli ultimi voti di Filippo:
 
         publisher.handle(event, context)
 
-        # print(dir(notifier.send))
-
         notifier.send.assert_called_with(case['result'])
-
-#     mocker.patch.object(grade_scraper, "all")
-#     grade_scraper.all.side_effect = [
-#         {'00002401': [
-#                 Grade(when='22/01/2020', subject='GEOGRAFIA', type='Orale', value='9', comment='La collina.', teacher='Carmigno Alessandra'),
-#                 Grade(when='23/01/2020', subject='GEOGRAFIA', type='Orale', value='10', comment='La collina.', teacher='Carmigno Alessandra'),
-#             ]},
-#         # {'00002138': [Grade(when='22/01/2020', subject='GEOGRAFIA', type='Orale', value='9', comment='La collina.', teacher='Carmigno Alessandra')]}
-#     ]
-
-#     mocker.patch.object(student_repository, "all")
-#     student_repository.all.side_effect = [
-#         [Student(
-#             '00002401', "Filippo", "Branca",
-#             [Grade(when='22/01/2020', subject='GEOGRAFIA', type='Orale', value='9', comment='La collina.', teacher='Carmigno Alessandra')],
-#             '2020-01-26T09:05:35.692104',
-#             '2020-01-26T09:05:35.692104'
-#         )]
-        
-#     ]
-
-#     mocker.patch.object(notifier, "send")
-#     notifier.send.side_effect = None
-
-#     # notifier_spy = mocker.spy(notifier, "send")
-
-#     publisher = PublishLatestGradesHandler(
-#         student_repository,
-#         grade_scraper,
-#         notifier
-#     )
-
-#     event = {
-#         "account": "123456789012",
-#         "region": "us-east-2",
-#         "detail": {},
-#         "detail-type": "Scheduled Event",
-#         "source": "aws.events",
-#         "time": "2019-03-01T01:23:45Z",
-#         "id": "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
-#         "resources": [
-#             "arn:aws:events:us-east-1:123456789012:rule/my-schedule"
-#         ]
-#     }
-#     context = {}
-
-#     publisher.handle(event, context)
-
-#     # print(dir(notifier.send))
-
-#     notifier.send.assert_called_with(
-#         """Ecco gli ultimi voti di Filippo:
-
-# - 23/01/2020: 10 di GEOGRAFIA da Carmigno Alessandra (La collina.)
-# """)
-
-#     # notifier_spy.assert_called_once()
-#     # # assert len(notifier_spy.spy_return) > 0
-
-#     # print(dir(notifier_spy))
-#     # print(dir(notifier_spy.return_value))
-#     # print(notifier_spy.call_args)
-#     # print(notifier_spy.call_count)
-#     # print(notifier_spy.spy_return.return_value)
-#     # print(notifier_spy.side_effect)
-
-#     # assert notifier_spy.return_value == 'stocazzo'
-
-#     # print("spy_return", notifier_spy.spy_return)
